41397169ca599572974cfada4afa31a113dd586f
Vinay S Shenoy <vinay@obvious.in>
2019-05-06 10:21:44 +0000

    Delay subscription to view change event when binding Ui to controller

--- app/src/main/java/org/simple/clinic/ViewControllerBinding.kt
+++ app/src/main/java/org/simple/clinic/ViewControllerBinding.kt
@@ -7,19 +7,32 @@ import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import io.reactivex.schedulers.Schedulers.io
 import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
+import org.threeten.bp.Duration
+import java.util.concurrent.TimeUnit
 
 @SuppressLint("CheckResult")
 fun <T> bindUiToController(
     ui: T,
     events: Observable<UiEvent>,
     controller: ObservableTransformer<UiEvent, (T) -> Unit>,
-    screenDestroys: Observable<ScreenDestroyed>
+    screenDestroys: Observable<ScreenDestroyed>,
+    uiChangeDelay: Duration = Duration.ZERO
 ) {
   events
       .mergeWith(screenDestroys)
       .observeOn(io())
       .compose(controller)
+      .compose(delayEmissionsBy(uiChangeDelay))
       .observeOn(mainThread())
       .takeUntil(screenDestroys)
       .subscribe { uiChange -> uiChange(ui) }
+}
+
+private fun <T> delayEmissionsBy(duration: Duration): ObservableTransformer<(T) -> Unit, (T) -> Unit> {
+  return ObservableTransformer { upstream ->
+    when (duration) {
+      Duration.ZERO -> upstream
+      else -> upstream.delaySubscription(duration.toMillis(), TimeUnit.MILLISECONDS)
+    }
+  }
 }
